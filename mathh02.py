ab177b1011b2d9bd8f19ffb3a3a58a9740771eb4
xuanting09 <0912shenny@gemail.com>
2022-10-14 09:55:57 +0800

    math

--- mathh02.py
+++ mathh02.py
@@ -1,31 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
- Creating vectors X and Y
+ #Creating vectors X and Y
 x = np.linspace(-2,2,100)
 y = x ** 2
 
 fig = plt.figure(figsize = (12, 7))
 
- Add a title
+ #Add a title
 plt.title('Equation plot')
 
- Add X and y Label
+ #Add X and y Label
 plt.xlabel('x axis')
 plt.ylabel('y axis')
 
- Create the plot
+ #Create the plot
 plt.plot(x, y, label ='Y = X^2',
     color ='red', linewidth = 1)
 
 plt.plot(x, 6 * y, label ='Y = 6 * X^2',
     color ='yellow', linewidth = 1)
 
- Add a grid
+ #Add a grid
 plt.grid(linestyle ='-')
 
- Add a Legend
+ #Add a Legend
 plt.legend()
 
- Show the plot
+ #Show the plot
 plt.show()
