f088489ca260a0987f056d4a5eec81c2b298e5f0
xuanting09 <0912shenny@gemail.com>
2022-10-20 15:31:11 +0800

    10200331

--- ex002.py
+++ ex002.py
@@ -1,25 +1,8 @@
-a=input()
-b=input()
-bb = [int(b) for b in str(b)]
-aa = [int(a) for a in str(a)]
-a=0
-if aa[1]==aa[3] or aa[1]!=aa[5] or bb[1]==bb[3] or bb[1]!=bb[5] :
-    a=a+1
-if aa[6]==1 or bb[6]==0:
-    a=a+2
-if aa[1]==bb[1] or aa[3]==bb[3] or aa[5]==bb[5] :
-    a=a+3
-if a==1:
-    print('A')
-if a==3:
-    print('AB')
-if a==4:
-    print('AC')
-if a==5:
-    print('BC')
-if a==6:
-    print('ABC')
-if a==0:
-    print('None')
+from re import I
 
-      
+
+x=int(input())
+y=1
+for i in range(1,x+1,1):
+    y=y*i
+print(y)
