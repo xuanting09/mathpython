4edfcf191335b60e91dbad884717cbc36d92ed84
xuanting09 <0912shenny@gemail.com>
2022-10-21 12:00:42 +0800

    20221021math

---
+++ ex004vectors.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+a=np.array([3,-4])
+a1=np.array([4,3])
+b=np.array([2,2,-1])
+b1=np.array([-1,2,2])
+c=np.array([4,5,-6])
+c1=np.array([0,-2,-3])
+print('ab',np.dot(a,a1))
+
--- mathh03.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-a=np.array([2,4,9,3,1])
-b=np.array([4,1,2,0,5])
-c=np.array([2,4,3,9,6])
-print('a+b',a+b)
-print('b+a',b+a)
-print('(a+b)+c',(a+b)+c)
-print('a+(b+c)',a+(b+c))
